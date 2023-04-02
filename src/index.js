0fcf5883be638ead59cba662863b574e64ee01a8
riya408 <122002330+riya408@users.noreply.github.com>
2023-04-02 21:29:17 +0530

    updated src

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { RecoilRoot } from 'recoil';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Router from "./Router/Router";
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import { RecoilRoot } from "recoil";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RecoilRoot>
-    <App />
+      <BrowserRouter> 
+        <Router />
+      </BrowserRouter>
     </RecoilRoot>
   </React.StrictMode>
 );
